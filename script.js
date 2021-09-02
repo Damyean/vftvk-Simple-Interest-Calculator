8b3a5aa1d0d7405777b3a52cbd946e88439f74fd
Damyean <89228226+Damyean@users.noreply.github.com>
2021-09-02 15:43:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-function handleSubmit(event)
-{
-    event.preventDefault();
-    
-    var p = document.getElementById("principal").value;
-    
-    if(p<=0) {
-        
-        document.getElementById("simpleintrest").innerHTML = ": " + " :  " + "Value of Principal Cannot be or zero negative";
-        alert("Value cannot be negative or zero")
-    }
-    else if (p>0){
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 
-    
-        var r = document.getElementById("rate").value;
-    var t = document.getElementById("time").value;
-    var r1 = r/100;
-    var t1 = t/12;
-    var si = p*r1*t1;
-    document.getElementById("simpleintrest").innerHTML = ": " + " :  " + si;
-    }
-    
 }
 
-function handleChange(){
-    
-    var r = document.getElementById("rate").value;
-    document.getElementById("change").innerHTML = r+"%";
+//update ther ate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    if (principal==""){
+       alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
